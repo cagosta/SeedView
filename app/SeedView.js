571dbf95b240b35d1d1efc1a28d697047c1d2994
cyril agosta <cyril.agosta@gmail.com>
2014-01-26 20:45:01 +0100

    Releasing v0.0.18

--- app/SeedView.js
+++ app/SeedView.js
@@ -1,5 +1,5 @@
 /**
- * SeedView version: "0.0.17" Copyright (c) 2011-2012, Cyril Agosta ( cyril.agosta.dev@gmail.com) All Rights Reserved.
+ * SeedView version: "0.0.18" Copyright (c) 2011-2012, Cyril Agosta ( cyril.agosta.dev@gmail.com) All Rights Reserved.
  * Available via the MIT license.
  * see: http://github.com/cagosta/SeedView for details
  */
